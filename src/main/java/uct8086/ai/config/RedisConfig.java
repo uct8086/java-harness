c72859aa52e1946dacf50a74419d19dadb59cd0f
Cruise <belieftang@163.com>
2026-07-18 16:47:47 +0800

    fix: bug

--- src/main/java/uct8086/ai/config/RedisConfig.java
+++ src/main/java/uct8086/ai/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package uct8086.ai.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +13,6 @@ public class RedisConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        return new ObjectMapper().registerModule(new JavaTimeModule());
     }
 }
